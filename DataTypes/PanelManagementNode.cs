a4fc7dcc79536aa81edf5eb4c66cb74c2f52e5e8
C-Tec R&D <c-tec.apps@C-Tec.co.uk>
2025-02-05 11:28:24 +0000

    Delete DataTypes directory

--- DataTypes/PanelManagementNode.cs
+++
@@ -1,9 +0,0 @@
-﻿//namespace Xfp.DataTypes
-//{
-//    internal class PanelManagementNode
-//    {
-//        internal int    Number { get; set; }
-//        internal string Name => string.Format(Cultures.Resources.Panel_x, Number);
-//        internal bool   IsPresent { get; set; }
-//    }
-//}
