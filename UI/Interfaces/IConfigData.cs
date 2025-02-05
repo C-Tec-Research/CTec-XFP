7347b14f0bf416f21f92436abc82fe16b26b0fdb
C-Tec R&D <c-tec.apps@C-Tec.co.uk>
2025-02-05 11:23:57 +0000

    Add files via upload

---
+++ UI/Interfaces/IConfigData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xfp.DataTypes.PanelData;
+
+namespace Xfp.UI.Interfaces
+{
+    public interface IConfigData
+    {
+        ConfigErrorPage GetPageErrorDetails();
+
+        void ClearErrors();
+
+        bool HasErrorsOrWarnings();
+
+        bool HasErrors();
+
+        bool HasWarnings();
+    }
+}
