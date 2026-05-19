dd79c43e699558d8a0e7baa02097f358ea746c70
JamieBartleet <jamie.bartleet@gmail.com>
2026-05-19 17:59:45 +0100

    260519 save
    260519 save

--- XFP/DataTypes/PanelData/EventLogData.cs
+++ XFP/DataTypes/PanelData/EventLogData.cs
@@ -2,18 +2,18 @@
 {
     public partial class EventLogData
     {
-        //public static string LogText { get; set; }
+        public static string LogText { get; set; }
 
-        ///// <summary>
-        ///// Path of the last-read comms log file, if any.
-        ///// </summary>
-        //public static string FilePath { get; set; }
-
-
-        //public static void Clear() => FilePath = LogText = null;
+        /// <summary>
+        /// Path of the last-read comms log file, if any.
+        /// </summary>
+        public static string FilePath { get; set; }
 
 
-        //public delegate void ViewerClearer();
-        //public static ViewerClearer ClearEventLogViewer;
+        public static void Clear() => FilePath = LogText = null;
+
+
+        public delegate void ViewerClearer();
+        public static ViewerClearer ClearEventLogViewer;
     }
 }
